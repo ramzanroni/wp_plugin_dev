393a618f695723016e51c5a3f642ea27fa6b108a
ramzanroni <mdramzanroni76@gmail.com>
2023-07-07 12:32:22 +0600

    step of active deactive and delete

--- wp-content/plugins/alecaddd-plugin/alecaddd-plugin.php
+++ wp-content/plugins/alecaddd-plugin/alecaddd-plugin.php
@@ -32,11 +32,23 @@ defined('ABSPATH') or die('Hey, You cannot access this file');
 
 class AlecadddPlugin
 {
-    function __construct($string)
-    {
-        echo $string;
+    function alecadddPlugin_activated(){
+       // generated a CPT
+       // flush rewrite rules
+    }
+    function alecadddPlugin_deacticated(){
+       // flush rewrite rules
+    }
+    function alecadddPlugin_uninstall(){
+            //delete CPT
+            // delete all the plugin data form DB
     }
 }
 if(class_exists('AlecadddPlugin')){
-    $alecadddPlugin=new AlecadddPlugin('Hello Plugin');
+    $alecadddPlugin=new AlecadddPlugin();
 }
+
+// activation 
+register_activation_hook(__FILE__, array($alecadddPlugin, 'alecadddPlugin_activated'));
+// deactivation
+register_deactivation_hook(__FILE__, array($alecadddPlugin, 'alecadddPlugin_deacticated'));
